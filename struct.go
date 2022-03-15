6c5e274d47491cafe4477e2186f68ad06a1796e5
anilask129 <96100135+anilask129@users.noreply.github.com>
2022-03-15 09:04:13 +0530

    added rooll

--- struct.go
+++ struct.go
@@ -7,10 +7,11 @@ type User struct {
 	name       string
 	occupation string
 	age        int
+	rooll      int
 }
 
 func main() {
-	u := User{"john Doe", "gardender", 43}
+	u := User{"john Doe", "gardender", 43,1234}
 
-	fmt.Printf("%s  is %d years old and he is a %s\n", u.name, u.name, u.occupation)
+	fmt.Printf("%s  is %d years old and he is a %s\n", u.name, u.name, u.occupation,u.rooll)
 }
